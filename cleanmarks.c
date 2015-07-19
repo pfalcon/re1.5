1cce743a9d9cd6e89a512c7f2fa3191e49ebe7ea
Amir Plivatsky <amirpli@gmail.com>
2015-07-19 16:00:45 +0300

    cleanmarks: Add missing instructions.

--- cleanmarks.c
+++ cleanmarks.c
@@ -12,12 +12,27 @@ cleanmarks(ByteProg *prog)
     while (pc < end) {
         *pc &= 0x7f;
         switch (*pc) {
+        case Class:
+        case ClassNot:
+            pc += (unsigned char)pc[1] * 2;
+        case NamedClass:
         case Jmp:
         case Split:
         case RSplit:
         case Save:
         case Char:
             pc++;
+            break;
+#ifdef DEBUG
+        case Bol:
+        case Eol:
+        case Any:
+        case Match:
+            break;
+        default:
+            printf("Unknown instruction 0x%02x pc %ld\n", (unsigned char)*pc, pc - prog->insts);
+            re1_5_fatal("cleanmarks");
+#endif
         }
         pc++;
     }
